59fd66f6e8b40866db640e475213913222a1cb7d
art <aten@udel.edu>
2019-09-08 19:49:32 -0400

    learned something

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-$(function(){
-    //this code runs after page is fully loaded
+$(function(){ // short for $(document).ready() 
+   // alert("ready!");
+    //recall that a form also has input types, in this case, a button!
+    $("#nameform").on('submit',function(event) { // using id selector
+        alert("Submitted");
+        event.preventDefault();
+        //regular js way
+        //var firstName = document.getElementById("fname").Value;
+        //var lastName = document.getElementById("lname").value;
+        //with jquery
+        var firstName=$('#fname').val();     
+        var lastName=$('#lname').val();  
+        var newHTML = '<h1>'+firstName+' '+lastName+'</h1>';
+
+        $(".jumbotron").removeClass('d-none').html(newHTML);
+        return false;
     });
+
+});
